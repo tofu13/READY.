2e86392a939ab34906fe2e090efa118d2ca276c7
marc8 <brtmrc@gmail.com>
2021-03-07 16:48:02 +0100

    Remove some command line options

--- READY..py
+++ READY..py
@@ -7,23 +7,25 @@ from config import *
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename")
+    parser = argparse.ArgumentParser(
+        description="An educational C=64 emulator."
+    )
+    parser.add_argument("filename", action='store', default=None, nargs="?",
+                        help="Binary object to be run. If missing boots normally.")
     parser.add_argument("-1", "--cbm-format", action='store_true',
                         help="First two bytes of file are little endian load address (like in LOAD\"*\",8,1)")
     #parser.add_argument("-a", "--assembly", action='store_true',
     #                    help=f"Compile and run an assembler file using compiler specified with"
     #                         f" -c (default: {DEFAULT_COMPILER}). See config.")
-    parser.add_argument("-c", "--compiler", action='store', default=f"{DEFAULT_COMPILER}",
-                        help=f"Use this compiler. Available: {', '.join(COMPILERS.keys()) or 'none :( '}. See config.")
+    #parser.add_argument("-c", "--compiler", action='store', default=f"{DEFAULT_COMPILER}",
+    #                    help=f"Use this compiler. Available: {', '.join(COMPILERS.keys()) or 'none :( '}. See config.")
     parser.add_argument("-s", "--load-address", action='store',
                         help=f"Load binary file at address (if not specified: ${DEFAULT_LOAD_ADDRESS:04X}). "
                              f"Use (escaped)$ or 0x for hex value.")
-    parser.add_argument("-n","--no-roms", action='store_true', default=False,
-                        help="Do not load roms")
+    #parser.add_argument("-n","--no-roms", action='store_true', default=False,
+    #                    help="Do not load roms")
     args = parser.parse_args()
 
-    print(args)
     if args.cbm_format and args.load_address:
         raise parser.error("-1/--cbmformat and -s/--load-addres conflicts. Choose one.")
 
@@ -41,7 +43,8 @@ if __name__ == '__main__':
             raise parser.error(f"Invalid load address {args.load_address}")
 
     base = None
-    roms = None if args.no_roms else hardware.roms.ROMS(ROMS_FOLDER)
+    #roms = None if args.no_roms else hardware.roms.ROMS(ROMS_FOLDER)
+    roms = hardware.roms.ROMS(ROMS_FOLDER)
 
     c64 = hardware.machine.Machine(
         hardware.memory.BytearrayMemory(65536),
@@ -50,8 +53,9 @@ if __name__ == '__main__':
         roms,
     )
 
-    if True:
+    if args.filename:
         base = c64.load(args.filename, base or DEFAULT_LOAD_ADDRESS, args.cbm_format)
         c64.cpu.run(base)
 
-        pass
+    else:
+        c64.cpu.run(0xFCE2)
