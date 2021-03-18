b2a1e967fa77681ebc5be8b05415cc5047b66d30
marc8 <brtmrc@gmail.com>
2021-03-18 19:33:59 +0100

    Add keyboard scan (tentative)

--- hardware/cia.py
+++ hardware/cia.py
@@ -14,45 +14,63 @@ class CIAA:
 
     def init(self):
         self.memory.read_watchers.append((0xDC00, 0xDCFF, self.get_registers))
-        self.memory.write_watchers.append((0xDC00, 0xDCFF, self.set_registers))
+        #self.memory.write_watchers.append((0xDC00, 0xDCFF, self.set_registers))
 
     def loop(self, memory):
         while True:
             self.pipe.send("IRQ")
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.mod & pygame.KMOD_LSHIFT:
-                        selector = 1
-                    elif event.mod & pygame.KMOD_LALT:
-                        selector = 2
-                    else:
-                        selector = 0
-                    key = KEYTABLE.get(event.key)
-                    if key:
-                        key = key[selector]
-                    if key:
-                        # Inject char into keyboard buffer
-                        #memory[0x277 + memory[0xC6]] = key
-                        # Update counter
-                        #memory[0xC6] += 1
+            time.sleep(.4)
 
-                        row, col = KEYSCAN.get(event.key, [8,8])
-                        self.keyboard_row = 0xFF - 2 ** row
-                        self.keyboard_col = 0xFF - 2 ** col
+            # for event in pygame.event.get():
+            #     if event.type == pygame.KEYDOWN:
+            #         if event.mod & pygame.KMOD_LSHIFT:
+            #             selector = 1
+            #         elif event.mod & pygame.KMOD_LALT:
+            #             selector = 2
+            #         else:
+            #             selector = 0
+            #         key = KEYTABLE.get(event.key)
+            #         if key:
+            #             key = key[selector]
+            #         if key:
+            #             # Inject char into keyboard buffer
+            #             memory[0x277 + memory[0xC6]] = key
+            #             # Update counter
+            #             memory[0xC6] += 1
+            #
+            #             #row, col = KEYSCAN.get(event.key, [8,8])
+            #             #self.keyboard_row = 0xFF - 2 ** row
+            #             #self.keyboard_col = 0xFF - 2 ** col
+            #
+            #             #print ("READ K",self.keyboard_row, self.keyboard_col)
+            #
+            #
+            #     elif event.type == pygame.KEYUP:
+            #         self.memory[0xDC01] = 0xFF #No key pressed
 
 
-                elif event.type == pygame.KEYUP:
-                    self.memory[0xDC01] = 0xFF #No key pressed
-
-
-            time.sleep(.2)
 
 
     def get_registers(self, address, value):
         #print("read ciaA", address)
         if address == 0xDC01:
-            if self.memory[0xDC00] == self.keyboard_row:
-                return self.keyboard_col
+            key = pygame.key.get_pressed()
+            row = self.memory[0xDC00]
+            if row == 0xFE:
+                if key[pygame.K_DELETE] or key[pygame.K_BACKSPACE]:
+                    return 0xFE
+                elif key[pygame.K_RETURN]:
+                    return 0xFD
+                elif key[pygame.K_RIGHT]:
+                    return 0xFB
+            elif row == 0xFD:
+                if key[pygame.K_3]:
+                    return 0xFE
+                elif key[pygame.K_5]:
+                    return 0xFD
+                elif key[pygame.K_7]:
+                    return 0xFB
+
             else:
                 return 0xFF
         else:
@@ -62,7 +80,8 @@ class CIAA:
 
     def set_registers(self, address, value):
         #print("write ciaA", address, value)
-        self.memory.contents[address] = value
+        #self.memory.contents[address] = value
+        pass
 
 
 class CIAB:
