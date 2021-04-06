235786d1c7c65f50c7266abaa9b9c429907ef4f5
marc8 <brtmrc@gmail.com>
2021-04-06 23:29:02 +0200

    Add test program

---
+++ programs/color_bars2.asm
@@ -0,0 +1,50 @@
+        LDA #$00
+        STA $FB
+        STA $FD
+        LDA #$04
+        STA $FC
+        LDA #$D8
+        STA $FE
+
+        LDX #$00
+loop2   LDY #$27
+loop1   LDA #$A0
+        STA ($FB),Y
+        TXA
+        STA ($FD),Y
+        DEY
+        BPL loop1
+        CLC
+        LDA #$28
+        ADC $FB
+        STA $FB
+        STA $FD
+        BCC nocarry
+        INC $FC
+        INC $FE
+nocarry
+        INX
+        CPX #$19
+        BNE loop2
+
+        JSR $E544
+
+        LDA #$00
+        STA $FB
+        STA $FD
+        LDA #$04
+        STA $FC
+        LDA #$D8
+        STA $FE
+
+        LDY #$FF
+loop3
+        LDA ($FB),Y
+        ADC #$01
+        STA ($FB),Y
+        BNE loop3
+        DEY
+        BNE loop3
+        JSR $E544
+
+        BRK
