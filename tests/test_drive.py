103a9377e2ea9b6a21d9bb53b4096c5e43cf0d91
marc8 <brtmrc@gmail.com>
2024-05-04 00:20:17 +0200

    Update tests

--- tests/test_drive.py
+++ tests/test_drive.py
@@ -2,18 +2,19 @@ import pytest
 
 from hardware import disk_drive
 
+TEST_DISK_IMAGE = "tests/test_image.d64"
 
 @pytest.fixture
 def my_drive() -> disk_drive.Drive:
     drive = disk_drive.Drive()
-    drive.set_imagefile("test_image.d64")
+    drive.set_imagefile(TEST_DISK_IMAGE)
     return drive
 
 
 def test_drive_properties(my_drive):
     assert my_drive
 
-    assert my_drive.image_file == "test_image.d64"
+    assert my_drive.image_file == TEST_DISK_IMAGE
 
     my_drive.set_filename("foo/bar.d64")
     assert my_drive.filename == "foo/bar.d64"
@@ -25,5 +26,5 @@ def test_drive_properties(my_drive):
 def test_drive_read(my_drive):
     read = my_drive.read(b"HELLO")
 
-    assert len(read) == 24
-    assert read == b'\x01\x08\x15\x08\n\x00\x99 "HELLO WORLD"\x00\x00\x00'
+    assert len(read) == 36
+    assert read == b'\x01\x08!\x08\n\x00\x8b "FOO" \xb2 "BAR" \xa7 \x99 "SPAM!"\x00\x00\x00'
