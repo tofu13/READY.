e37dfceb503e2893507a3e93ff6dde2270997120
brtmrc@gmail.com <works77GIT>
2024-08-13 22:28:06 +0200

    Refactor: modulize parser

--- READY..py
+++ READY..py
@@ -2,45 +2,7 @@ import argparse
 
 import config
 from libs import hardware
-
-
-def create_parser():
-    parser = argparse.ArgumentParser(description="An educational C=64 emulator.")
-    parser.add_argument(
-        "-s",
-        "--screen",
-        action="store",
-        help=f"Screen driver (default: {config.SCREEN})",
-        default=config.SCREEN,
-        choices=["raster", "virtual", "fast"],
-    )
-    parser.add_argument(
-        "-d", "--disk", action="store", help="Disk (t64)", default="", type=str
-    )
-    parser.add_argument(
-        "-c",
-        "--console",
-        action="store_true",
-        help="Show screen in console (chars only)",
-        default=False,
-    )
-    parser.add_argument(
-        "-t", "--loadstate", action="store", help="Load state from file", default=False
-    )
-    parser.add_argument(
-        "-ar",
-        "--autorun",
-        action="store_true",
-        help="Autorun * from disk",
-        default=False,
-    )
-    parser.add_argument(
-        "-at",
-        "--autotype",
-        action="store",
-        help="Autotype command. Use | for return key",
-    )
-    return parser
+from libs.parser import create_parser
 
 
 def main():
